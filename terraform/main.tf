dd23a05086e0e056e454c1f56f466f4f48c339f1
r0naldy <ronaldyitolopez@gmail.com>
2025-06-29 00:45:34 -0500

    Arreglado conflicto con Instance Profile y actualizada infraestructura

--- terraform/main.tf
+++ terraform/main.tf
@@ -54,7 +54,7 @@ resource "aws_security_group" "allow_http" {
   }
 }
 
-# IAM Role para EC2 con acceso a S3.
+# IAM Role para EC2 con acceso a S3
 resource "aws_iam_role" "ec2_role" {
   name = "ec2_s3_access_role"
 
